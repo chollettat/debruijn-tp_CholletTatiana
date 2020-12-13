39b4e284446ce2992bedb71dad7c0fb7bcaeed70
Tatiana Chollet <chollettatiana@gmail.com>
2020-12-13 16:55:26 +0100

    Parcours du graph de Bruijn

--- debruijn/debruijn.py
+++ debruijn/debruijn.py
@@ -65,7 +65,15 @@ def get_arguments():
     return parser.parse_args()
 
 
+## Construction of the Bruijn graph
+# Unique k-mer identification
 def read_fastq(fastq_file):
+    """
+    Read the file
+    :Parameters:
+        fastq_file: file fastq
+    Returns: sequences generator
+    """
     file = open(fastq_file, 'r')
     for line in file:
         yield next(file).strip('\n') #We remove the line break of each line of file
@@ -74,19 +82,34 @@ def read_fastq(fastq_file):
 
 
 def cut_kmer(read, kmer_size):
+    """
+    ----------------------
+    :Parameters:
+        read: sequence
+        kmer_size: size of kmer
+    Returns: k-mer generator
+    """
     for comp in range ( (len(read)+1) - kmer_size):
         kmer = read[comp : comp + kmer_size]
         yield kmer
 
 
 def build_kmer_dict(fastq_file, kmer_size):
+    """
+    Dictionary creation
+    :Parameters:
+        fastq_file: file fastq
+        kmer_size: size of kmer
+    Returns: dictionary having for key the k-mer
+             and for value the number of occurrences of this k-mer
+    """
     dict_kmer = {} #We use {} to use a dictionnary
-    genSeq = read_fastq(fastq_file)
+    gen_seq = read_fastq(fastq_file)
 
-    for seq in genSeq:
-        genKmer = cut_kmer(seq, kmer_size)
+    for seq in gen_seq:
+        gen_kmer = cut_kmer(seq, kmer_size)
 
-        for kmer in genKmer:
+        for kmer in gen_kmer:
             if kmer in dict_kmer: #Count occurence for each kmer
                 dict_kmer[kmer] += 1
             else:
@@ -94,13 +117,22 @@ def build_kmer_dict(fastq_file, kmer_size):
 
     return dict_kmer
 
-#kmer1.pck and test_construction_debruijn.py were changed because I'm on Windows
-def build_graph(kmer_dict): #kmer_dict = dict_kmer from build_kmer_dict
-    treeKmer = nx.DiGraph()
-    for kmer, weight in kmer_dict.items():
-        treeKmer.add_edge(kmer[:-1], kmer[1:], weight=weight) #kmer[:-1] slice before  = prefix / kmer[1:] slice after = suffix
 
-    return treeKmer
+# Construction of the Bruijn tree
+#kmer1.pck and test_construction_debruijn.py have been modified to pass the tests because of Windows
+def build_graph(kmer_dict): #kmer_dict = dict_kmer from build_kmer_dict
+    """
+    Creation of an oriented and weighted tree
+    :Parameters:
+        kmer_dict: kmer dictionary
+    Returns: k-mers tree prefixes and suffixes
+    """
+    tree_kmer = nx.DiGraph()
+    for kmer, weight in kmer_dict.items():
+        #kmer[:-1] slice before  = prefix / kmer[1:] slice after = suffix
+        tree_kmer.add_edge(kmer[:-1], kmer[1:], weight=weight)
+
+    return tree_kmer
 
 
 def remove_paths(graph, path_list, delete_entry_node, delete_sink_node):
@@ -110,7 +142,7 @@ def std(data):
     pass
 
 
-def select_best_path(graph, path_list, path_length, weight_avg_list, 
+def select_best_path(graph, path_list, path_length, weight_avg_list,
                      delete_entry_node=False, delete_sink_node=False):
     pass
 
@@ -129,22 +161,105 @@ def solve_entry_tips(graph, starting_nodes):
 def solve_out_tips(graph, ending_nodes):
     pass
 
+
+## Path of the graph of Bruijn
 def get_starting_nodes(graph):
-    pass
+    """
+    Indentify startings nodes
+    :Parameters:
+        graph: oriented and weighted tree (nx.digraph)
+    Returns: list of starting nodes
+    """
+    starting_nodes = []
+    for node in graph.nodes:
+        predess = graph.predecessors(node) #Returns an iterator over predecessor nodes of n
+        if not list(predess): #if empty list
+            starting_nodes.append(node)
+    return starting_nodes
+
 
 def get_sink_nodes(graph):
-    pass
+    """
+    Indentify sink nodes
+    :Parameters:
+        graph: oriented and weighted tree (nx.digraph)
+    Returns: list of sink nodes
+    """
+    sink_nodes = []
+    for node in graph.nodes:
+        successor = graph.successors(node) #Returns an iterator over successor nodes of n
+        if not list(successor): #if empty list
+            sink_nodes.append(node)
+    return sink_nodes
+
 
 def get_contigs(graph, starting_nodes, ending_nodes):
-    pass
+    """
+    Find contigs in a graph
+    :Parameters:
+        graph: oriented and weighted tree (nx.digraph)
+        starting_nodes : list of starting nodes
+        ending_nodes : list of ending nodes
+    Returns: tuple list (contig, contig size)
+    """
+    contigs_list = []
+    for input_node in starting_nodes:
+        for output_node in ending_nodes:
+            for path in nx.all_simple_paths(graph, source= input_node, target = output_node):
+                contig=path[0] #first node
+                for comp in path[1:]: #path without first element
+                    contig += comp[1:] #add nodes to contig
+                contigs_list.append((contig,len(contig)))
+    return contigs_list
 
-def save_contigs(contigs_list, output_file):
-    pass
 
 
 def fill(text, width=80):
     """Split text with a line return to respect fasta format"""
     return os.linesep.join(text[i:i+width] for i in range(0, len(text), width))
+
+
+def save_contigs(contigs_list, output_file): #AssertionError in tests with Windows
+    """
+    Save contigs in fasta format
+    :Parameters:
+        contigs_list: tuple list (contig, contig size)
+        output_file : output file
+    Returns: /
+    """
+    file = open(output_file, "w")
+    index = 0
+    for contig, sizeOfContig in contigs_list:
+        file.write(">contig_"+str(index)+" len="+str(sizeOfContig)+"\n")
+        file.write(fill(contig, width=80)+"\n")
+        index += 1
+
+
+def draw_graph(graph, graphimg_file):
+    """
+    Draw the graph
+    :Parameters:
+        graph: oriented and weighted tree (nx.digraph)
+        graphimg_file: file name .png
+    Returns: /
+    """
+    fig, ax = plt.subplots()
+    elarge = [(u,v) for (u,v,d) in graph.edges(data=True) if d['weight'] > 3]
+    #print(elarge)
+    esmall = [(u,v) for (u,v,d) in graph.edges(data=True) if d['weight'] <= 3]
+    #print(esmall)
+    #pos = nx.spring_layout(graph)
+    pos = nx.random_layout(graph)
+    nx.draw_networkx_nodes(graph, pos, node_size=6)
+    nx.draw_networkx_edges(graph, pos, edgelist=elarge, width=6)
+    nx.draw_networkx_edges(graph, pos, edgelist=esmall, width=6, alpha=0.5,
+                edge_color='b', style='dashed')
+    #nx.draw_networkx(graph, pos, node_size=10, with_labels=False)
+    # save image
+    plt.savefig(graphimg_file)
+
+
+
 
 #==============================================================
 # Main program
@@ -155,6 +270,23 @@ def main():
     """
     # Get arguments
     args = get_arguments()
+
+    # Variable declarations
+    fastq_file = args.fastq_file
+    kmer_size = args.kmer_size
+    output_file = args.output_file
+
+    # Build the graph
+    kmer_dict = build_kmer_dict(fastq_file,kmer_size)
+    graph = build_graph(kmer_dict)
+
+    #
+    starting_nodes = get_starting_nodes(graph)
+    sink_nodes = get_sink_nodes(graph)
+
+    # Get then save contigs
+    contigs_list = get_contigs(graph, starting_nodes, sink_nodes)
+    save_contigs(contigs_list,output_file)
 
 if __name__ == '__main__':
     main()
